94304cc6067e816a04a66525542ffebd9c1df6a1
Luis Crespo <lcrespom@gmail.com>
2022-05-11 21:21:14 +0200

    Supporting config.json

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,39 @@
+import fs from 'node:fs'
+
 import { registerHotkey } from './hotkey'
 import clipboard from 'clipboardy'
 import robot from 'robotjs'
 
-registerHotkey('V', ['LEFT ALT', 'LEFT CTRL'], () => {
+function handlePaste() {
     let clipboardTxt = clipboard.readSync()
     console.log('--- Pasting from clipboard ---')
     console.log(clipboardTxt)
     console.log('---')
+    typeText(clipboardTxt)
+}
+
+function typeText(txt: string) {
     setTimeout(() => {
-        robot.typeString(clipboardTxt)
+        //TODO split \n into lines and type ENTER explicitly
+        robot.typeString(txt)
     }, 250)
-})
+}
+
+function readConfig() {
+    try {
+        let config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
+        for (let preset of config.presets) {
+            registerHotkey(preset.key, preset.modifiers, () => typeText(preset.message))
+        }
+    } catch (e) {
+        console.warn('Warning: config.json not found or invalid')
+    }
+}
+
+function main() {
+    robot.setKeyboardDelay(10)
+    registerHotkey('V', ['LEFT ALT', 'LEFT CTRL'], handlePaste)
+    readConfig()
+}
+
+main()
