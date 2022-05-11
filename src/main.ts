e840f393ada8e1ba83210b5897b538aac35dd498
Luis Crespo <lcrespom@gmail.com>
2022-05-11 15:10:55 +0200

    Using robotjs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { registerHotkey } from './hotkey'
 import clipboard from 'clipboardy'
+import robot from 'robotjs'
 
 registerHotkey('V', ['LEFT ALT', 'LEFT CTRL'], () => {
+    let clipboardTxt = clipboard.readSync()
     console.log('--- Pasting from clipboard ---')
-    console.log(clipboard.readSync())
+    console.log(clipboardTxt)
     console.log('---')
+    setTimeout(() => {
+        robot.typeString(clipboardTxt)
+    }, 1000)
 })
